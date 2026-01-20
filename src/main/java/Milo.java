ba0283ed474254cf4e4c3613b87dbef63a55a693
nazeeeef007 <muhdnazeef007@gmail.com>
2026-01-20 16:27:05 +0800

    Level-1: Add echo support

--- src/main/java/Milo.java
+++ src/main/java/Milo.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
+
 public class Milo {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String name = "Milo";
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm " + name);
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
+
+        while (true) {
+            String input = scanner.nextLine();
+            System.out.println("____________________________________________________________");
+            if (input.equals("bye")) {
+                break;
+            }
+            System.out.println(" " + input);
+            System.out.println("____________________________________________________________");
+        }
+
         System.out.println(" Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
     }
