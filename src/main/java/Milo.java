e7ab08dba95ceb45bfaefe151b017145d9933c54
nazeeeef007 <muhdnazeef007@gmail.com>
2026-01-20 16:05:46 +0800

    Level-0: Rename to Milo and add greeting

---
+++ src/main/java/Milo.java
@@ -0,0 +1,11 @@
+public class Milo {
+    public static void main(String[] args) {
+        String name = "Milo";
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm " + name);
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
