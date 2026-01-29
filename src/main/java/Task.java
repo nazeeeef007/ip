08001661a66b117079bc4ff4f940c06a9fc06ca7
nazeeeef007 <muhdnazeef007@gmail.com>
2026-01-29 11:17:55 +0800

    Level-7: Add save and load functionality for Level-7

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -18,4 +18,8 @@ public class Task {
     public String toString() {
         return "[" + getStatusIcon() + "] " + description;
     }
+
+    public String toFileFormat() {
+        return (isDone ? "1" : "0") + " | " + description;
+    }
 }
