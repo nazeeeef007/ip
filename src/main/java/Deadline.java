2a583e56dfb84ac3fefd28da9b5dd0c624284458
nazeeeef007 <muhdnazeef007@gmail.com>
2026-01-29 11:40:32 +0800

    Level-8: Implement LocalDate for deadlines and events

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,16 +1,25 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
-    protected String by;
+    protected LocalDate by;
+
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        // trim() is important to remove accidental spaces around the date
+        this.by = LocalDate.parse(by.trim());
     }
+
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " +
+                by.format(DateTimeFormatter.ofPattern("MMM dd yyyy")) + ")";
     }
 
     @Override
     public String toFileFormat() {
         return "D | " + super.toFileFormat() + " | " + by;
     }
+
+    public LocalDate getBy() { return this.by; }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,20 +1,27 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
-    protected String from;
-    protected String to;
+    protected LocalDate from;
+    protected LocalDate to;
 
     public Event(String description, String from, String to) {
         super(description);
-        this.from = from;
-        this.to = to;
+        this.from = LocalDate.parse(from.trim());
+        this.to = LocalDate.parse(to.trim());
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
+        return "[E]" + super.toString() + " (from: " +
+                from.format(DateTimeFormatter.ofPattern("MMM dd yyyy")) +
+                " to: " + to.format(DateTimeFormatter.ofPattern("MMM dd yyyy")) + ")";
     }
 
     @Override
     public String toFileFormat() {
         return "E | " + super.toFileFormat() + " | " + from + " | " + to;
     }
+
+    public LocalDate getFrom() { return this.from; }
 }
